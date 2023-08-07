e754ef1192c836fb2d71ed4efaf7b94b60530c21
vkraven <felixk@vkraven.com>
2023-08-07 14:11:22 -0400

    Update default.nix for legacy builds

--- default.nix
+++ default.nix
@@ -1,17 +1,19 @@
 { mkDerivation, aeson, base, binary, bytestring, cmdargs
-, containers, dbus, directory, fdo-notify, lib, mtl, network
-, process, text, transformers, unix
+, containers, dbus, directory, fdo-notify, filepath, http-client
+, http-client-tls, lib, mtl, network, process, tabl, tagsoup, text
+, transformers, unix
 }:
 mkDerivation {
   pname = "swankybar";
-  version = "0.1.9.0";
+  version = "0.1.9.1";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
     aeson base binary bytestring cmdargs containers dbus directory
-    fdo-notify mtl network process text transformers unix
+    fdo-notify filepath http-client http-client-tls mtl network process
+    tabl tagsoup text transformers unix
   ];
-  description = "A Waybar custom plugin to toggle Adaptive Sync in Sway and display CPU scaling governors";
+  description = "A Waybar custom plugin to toggle Adaptive Sync in Sway and display CPU scaling governors, and other tools";
   license = lib.licenses.gpl3Only;
 }
